e2d4bdcd372ccfe3f4c2c6c12fe29c07252d940f
Denis Zhigun <zhigundv@gmail.com>
2021-01-09 20:31:49 +0300

    Update BonusService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,7 @@
 public class BonusMilesService {
-public int calculate (int price){int bonusMiles = price / 20;
-    return bonusMiles;}
+    public int calculate(int price) {
+        int bonusMiles = price / 20;
+        return bonusMiles;
+    }
 
 }
